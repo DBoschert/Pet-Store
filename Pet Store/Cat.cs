973e17bb8ae5cd1b35622276e5e7d842a41e4fe6
Drew Boschert <drew.r.boschert@gmail.com>
2023-08-21 16:17:40 -0400

    Add project files.

---
+++ Pet Store/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pet_Store
+{
+    internal class Cat : IBreedSpeak
+    {
+        public string Breed { get; set; }
+        public int Age { get; set; }
+        public string Speak { get; set; }
+
+    }
+}
---
+++ Pet Store/Dog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pet_Store
+{
+    internal class Dog : IBreedSpeak
+    {
+        public string Breed { get; set; }
+        public string Color { get; set; }
+        public string Speak { get; set; }
+
+
+    }
+}
